e01656f1f2b3c26880771c833070ba9d5cae4f26
Azra Ali <azra_ali09@outlook.com>
2026-05-17 22:16:20 -0500

    working on audio

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -3,6 +3,8 @@
 #include <WiFi.h>
 #include "soc/soc.h"
 #include "soc/rtc_cntl_reg.h"
+#include "driver/i2s.h"
+#include "Arena_Hall_1.h"
 
 const char* ssid = "SM-S901W1979";
 const char* password = "xkpj1427";
@@ -10,6 +12,10 @@ const char* password = "xkpj1427";
 WebSocketsServer webSocket = WebSocketsServer(81);
 
 
+const int I2S_LRC = 14;
+const int I2S_BCLK = 12;
+const int I2S_DATA = 13;
+const uint32_t total_samples = 99684; 
 
 const int buttons[3] = {4,32,21};
 const int leds[3] = {5,33,2};
@@ -18,7 +24,7 @@ const int interval = 1000;
 int timeout = 3000;
 long gameStart = 0;
 bool gameStarted = false;
-
+bool ledOn[3] = {false, false, false}
 unsigned long ran_go = 0;
 unsigned long previousMillis = 0;
 unsigned long lastPress[3] = {0,0,0};
@@ -28,7 +34,31 @@ unsigned long ledOnTime[3] = {0,0,0};
 bool reached[3] = {false,false, false};
 const long cooldown = 300;
 int ran = random(0, 3);
+void playTest(int frequency, int durationMs){
+  i2s_start(I2S_NUM_0); 
+  
+  size_t bytes_written;
+  int sample_rate = 44100;
+  const int buffer_size = 128;
+  int16_t dummy_samples[buffer_size];
+  
+  int period = sample_rate / frequency;
+  if (period == 0) return; 
 
+  for(int i = 0; i < buffer_size; i++){
+    dummy_samples[i] = ((i % period) < (period / 2)) ? 1500 : -1500;
+  }
+
+  int total_samples_needed = (sample_rate * durationMs) / 1000;
+  int loops = total_samples_needed / buffer_size;
+
+  for(int l = 0; l < loops; l++) {
+    i2s_write(I2S_NUM_0, dummy_samples, sizeof(dummy_samples), &bytes_written, portMAX_DELAY);
+  }
+
+  i2s_zero_dma_buffer(I2S_NUM_0); 
+  i2s_stop(I2S_NUM_0);
+}
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length){
   if(type==WStype_TEXT){
@@ -63,6 +93,32 @@ void setup(){
     pinMode(buttons[i], INPUT_PULLUP);
   }
   randomSeed(analogRead(0));
+  i2s_config_t i2s_config = {
+      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+      .sample_rate = 44100,
+      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+      .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+      .dma_buf_count = 8,
+      .dma_buf_len = 64,
+      .use_apll = false
+  };
+
+  i2s_pin_config_t pin_config = {
+      .bck_io_num = I2S_BCLK,
+      .ws_io_num = I2S_LRC,
+      .data_out_num = I2S_DATA,
+      .data_in_num = I2S_PIN_NO_CHANGE
+  };
+
+  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+  i2s_set_pin(I2S_NUM_0, &pin_config);
+
+  Serial.println("Testing Speaker hardware...");
+  playTest(880, 1000); 
+  delay(200);
+
   WiFi.begin(ssid, password);
   while(WiFi.status() !=WL_CONNECTED){
     delay(500);
@@ -101,11 +157,13 @@ void loop() {
     previousMillis = currentMillis;
     ran = random(0, 3);
     digitalWrite(leds[ran], HIGH);
+    ledOn[ran] = true;
     ledOnTime[ran] = currentMillis;
   }
   for(int i=0; i<3; i++){
     if(digitalRead(leds[i])==HIGH && currentMillis-ledOnTime[i] >=timeout){
       digitalWrite(leds[i], LOW);
+      ledOn[i] = false;
       score--;
       Serial.print("Missed, Score: ");
       Serial.println(score);
@@ -114,8 +172,9 @@ void loop() {
   for(int i=0; i<3; i++){
     if(digitalRead(buttons[i])==LOW && currentMillis-lastPress[i]>=cooldown){
       lastPress[i] = currentMillis;
-      if(digitalRead(leds[i])==HIGH){
+      if(ledOn[i]){
         digitalWrite(leds[i], LOW);
+        ledOn[i] = false;
         score++;
         Serial.print("Score: ");
         Serial.println(score);
